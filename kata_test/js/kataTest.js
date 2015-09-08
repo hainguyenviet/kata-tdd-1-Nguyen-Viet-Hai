ee7b69bcede66c04ca348ece72276543f2c672ae
hainguyenviet <viethai2002@gmail.com>
2015-09-08 10:42:56 +0700

    Update kataTest.js
    comit for solving question 4

--- kata_test/js/kataTest.js
+++ kata_test/js/kataTest.js
@@ -1,11 +1,40 @@
 
+function createDifferentDelimeters(numberString)
+{
+	var delimeters = ",";
+	
+	if(numberString.indexOf("//") > -1)
+		{
+			delimeters = numberString.split(/\n/)[0];
+			delimeters = delimeters.replace("//", "");
+		}
+	
+	return delimeters;
+}
+
+function replaceDeliNoti(numberString)
+{ 
+	if(numberString.indexOf("//") > -1)
+		{
+			numberString= numberString.replace(numberString.split(/\n/)[0], "");
+		}
+	
+	return numberString;
+}
+
 function Add(numberString) {
-  var listNum = numberString.split(/,|\n/);
+  var delimeters = createDifferentDelimeters(numberString);
+  var separators = [delimeters,"\n"];
+  numberString= replaceDeliNoti(numberString);
+  var listNum = numberString.split(new RegExp(separators.join('|'), 'g'));
   var res= 0;
   for(var i=0;i<listNum.length;i++)
       {
-        res +=parseInt(listNum[i]);
+	  if(listNum[i]!="")
+	  	{
+		  res +=parseInt(listNum[i]);
+	  	}
       }
-  alert(res);
+  return res;
 }
 
