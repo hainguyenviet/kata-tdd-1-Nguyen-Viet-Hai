ac52e787a9394d7af82b4e1ba09644d03998b632
hainguyenviet <viethai2002@gmail.com>
2015-09-08 09:47:27 +0700

    Create kataTest.js
    create js file

---
+++ kata_test/js/kataTest.js
@@ -0,0 +1,11 @@
+
+function Add(numberString) {
+  var listNum = numberString.split(/","|\n/);
+  var res= 0;
+  for(var i=0;i<listNum.length;i++)
+      {
+        res +=parseInt(listNum[i]);
+      }
+  alert(res);
+}
+
